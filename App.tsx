088d1311ee1ad47420756f2d6a3c13603153eea7
DeboraMdaGama <d.miranda.gama@gmail.com>
2021-10-21 10:48:55 -0300

    starting project

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+import Home from './src/pages/Home';
+
+
+const App = () => {
+
+  return (
+    <Home/>
+  );
+};
+
+
+export default App;
